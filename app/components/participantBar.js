d7749a311a0fe880a25b15ef2da8be2d51c107aa
Marshall Hampson <mhampson@liveaction.com>
2018-01-03 22:13:41 -0800

    Clean up and style, handle keydown event

--- app/components/participantBar.js
+++ app/components/participantBar.js
@@ -2,22 +2,33 @@
 
 function ParticpantBarController() {
   this.handleAdd = function() {
-    this.participants.push({
-      name: String(this.newParticipant)
-    })
-    console.log("Added!");
-  }
+    var parsedOffset = parseFloat(this.newParticipant.offset);
+    if (this.newParticipant.name && parsedOffset) {
+      this.participants.push({
+        name: String(this.newParticipant.name),
+        offset: this.newParticipant.offset
+      })
+    }
+  };
+
+  this.handleKeyUp = function ($event) {
+    if ($event.keyCode == 13) {
+      this.handleAdd();
+    }
+  };
 }
 
 angular.module('myApp')
 .component('participantBar', {
   template: `
+    <h1>Participants</h1>
     <ul>
       <li ng-repeat="participant in $ctrl.participants">
-        {{participant.name}} (UTC{{participant.offset}})
+        <span>{{participant.name}} (UTC {{participant.offset}})</span>
       </li>
-      <li>
-        <input type="text" ng-model="$ctrl.newParticipant" placeholder="Enter another ..."></input>
+      <li class="entry-field" ng-keyup="$ctrl.handleKeyUp($event)">
+        <input class="name-field" type="text" ng-model="$ctrl.newParticipant.name" placeholder="Name"></input>
+        <input class="offset-field" type="text" ng-model="$ctrl.newParticipant.offset" placeholder="Offset"></input>
         <button ng-click="$ctrl.handleAdd()">Add</button>
       </li>
     </ul>
