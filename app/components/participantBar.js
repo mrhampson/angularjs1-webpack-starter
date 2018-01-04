87ea962985a7d5afa8a351a59e787649ac039b66
Marshall Hampson <mhampson@liveaction.com>
2018-01-03 18:07:46 -0800

    Added ability to add names to list

--- app/components/participantBar.js
+++ app/components/participantBar.js
@@ -1,13 +1,27 @@
 'use strict';
 
+function ParticpantBarController() {
+  this.handleAdd = function() {
+    this.participants.push({
+      name: String(this.newParticipant)
+    })
+    console.log("Added!");
+  }
+}
+
 angular.module('myApp')
 .component('participantBar', {
   template: `
     <ul>
       <li ng-repeat="participant in $ctrl.participants">{{participant.name}}</li>
+      <li>
+        <input type="text" ng-model="$ctrl.newParticipant" placeholder="Enter another ..."></input>
+        <button ng-click="$ctrl.handleAdd()">Add</button>
+      </li>
     </ul>
   `,
   bindings: {
     participants: '='
-  }
+  },
+  controller: ParticpantBarController
 });
